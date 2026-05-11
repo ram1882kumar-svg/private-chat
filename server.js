63d77f313171a4b1f6395b276c1c3e9883344f9c
ram <ram1882kumar@gmail.com>
2026-05-11 20:19:44 +0530

    final fixed backend

--- server.js
+++ server.js
@@ -1,52 +1,55 @@
-const PASSWORD = "77068090";
 const express = require("express");
-const path = require("path");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
-const cors = require("cors");
 
 const app = express();
-app.use(cors());
-app.use(express.static(path.join(__dirname))); // serve index.html
-
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
-});
+const io = new Server(server);
 
-let usersInRoom = 0;
+const PASSWORD = "77068090";
+
+app.use(express.static(path.join(__dirname)));
+
+let users = 0;
 
 io.on("connection", (socket) => {
 
-  // login password
+  console.log("User connected");
+
   socket.on("login", (pass) => {
+
     if (pass !== PASSWORD) {
+      console.log("Wrong password");
       socket.disconnect();
+      return;
     }
-  });
 
-  // only 2 users allowed
-  if (usersInRoom >= 2) {
-    socket.disconnect();
-    return;
-  }
+    if (users >= 2) {
+      console.log("Room full");
+      socket.disconnect();
+      return;
+    }
 
-  usersInRoom++;
+    users++;
 
-  // message handling
-  socket.on("message", (msg) => {
-    socket.broadcast.emit("message", msg);
-  });
+    console.log("Login success");
 
-  // disconnect handling
-  socket.on("disconnect", () => {
-    usersInRoom--;
+    socket.on("message", (msg) => {
+      console.log("Message:", msg);
+
+      io.emit("message", msg);
+    });
+
+    socket.on("disconnect", () => {
+      users--;
+      console.log("User disconnected");
+    });
+
   });
 
 });
 
 server.listen(process.env.PORT || 3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server running");
 });
